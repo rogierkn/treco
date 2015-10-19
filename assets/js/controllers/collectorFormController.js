d1e37eaab34b4077e3b364826f1aef024d79daaa
Rogier <rogierkn@xs4all.nl>
2015-10-19 14:57:07 +0200

    Search params working, filter start

--- assets/js/controllers/collectorFormController.js
+++ assets/js/controllers/collectorFormController.js
@@ -1,95 +1,113 @@
-app.controller( 'collectorFormController', [ '$scope', '$state', function ( $scope, $state )
-{
+app.controller ('collectorFormController', ['$scope', '$state', function ($scope, $state) {
 
 
-  //$state.go('collector');
+    //$state.go('collector');
 
 
-  $scope.digest = {
-    keywords: [{ value: 'netherlands' }, { value: 'amsterdam' }],
-    searchParameters: {
-      resultsPerKeyword: 2,
-      sortMethod: {
-        value: 2,
-        options: ["relevance", "hot", "top", "new", "comments"]
-      },
-      timePeriod: {
-        value: 2,
-        options: ["hour", "day", "week", "month", "year", "all"]
-      }
-    },
-    filterRules: {
-      upvotesCount: 0,
-      subscriberCount: 0,
-      duplicates: {
-        filter: true,
-        merge: true,
-      },
-      custom: [
-        {term: 'european', title: true, body: true, username: true}
-      ]
-    },
-    categories: {
-      pictures: true,
-      gifs: true,
-      videos: true,
-      links: true,
-      self: true,
-    },
-    // Todo implement below
-    markup: {
-      categorise: false,
-      translations: {
-        on: 'on'
-      },
-      nonParticipatingLinks: true
-    },
-  };
+    $scope.digest = {
+        keywords: [{ value: 'netherlands' }, { value: 'amsterdam' }],
+        searchParameters: {
+            resultsPerKeyword: 25,
+            sortMethod: {
+                value: 2,
+                options: ["relevance", "hot", "top", "new", "comments"]
+            },
+            timePeriod: {
+                value: 2,
+                options: ["hour", "day", "week", "month", "year", "all"]
+            }
+        },
+        filterRules: {
+            upvotesCount: 500,
+            subscriberCount: 50000,
+            duplicates: {
+                filter: true,
+                merge: true,
+            },
+            custom: [
+                { term: 'european', title: true, body: true, username: true }
+            ]
+        },
+        categories: {
+            pictures: true,
+            gifs: true,
+            videos: true,
+            links: true,
+            self: true,
+        },
+        // Todo implement below
+        markup: {
+            categorise: false,
+            translations: {
+                on: 'on'
+            },
+            nonParticipatingLinks: true
+        },
+    };
 
 
-  $scope.log = function ( loggable )
-  {
-    console.log( loggable );
-  };
+    $scope.log = function (loggable) {
+        console.log (loggable);
+    };
 
 
-  // Keywords
-  $scope.createKeyword = function ()
-  {
-    if($scope.digest.keywords.length == 10) {
-      Materialize.toast('You can only use 10 keywords', 2000);
-    } else {
-      $scope.digest.keywords.push( { value: '' } );
-    }
-  };
-  $scope.removeKeyword = function ( index )
-  {
-    $scope.digest.keywords.splice( index, 1 );
-  };
-  $scope.keywordsCanContinue = function ()
-  {
-    if ( $scope.digest.keywords.length == 0 )
-    {
-      return false;
-    }
+    // Keywords
+    $scope.createKeyword       = function () {
+        if ($scope.digest.keywords.length == 10) {
+            Materialize.toast ('You can only use 10 keywords', 2000);
+        } else {
+            $scope.digest.keywords.push ({ value: '' });
+        }
+    };
+    $scope.removeKeyword       = function (index) {
+        $scope.digest.keywords.splice (index, 1);
+    };
+    $scope.keywordsCanContinue = function () {
+        if ($scope.digest.keywords.length == 0) {
+            return false;
+        }
 
-    for ( var i = 0; i < $scope.digest.keywords.length; i++ )
-    {
-      if ( $scope.digest.keywords[ i ] == '' )
-      {
-        return false;
-      }
-    }
-    return true;
-  };
-  $scope.keywordsContinue = function ()
-  {
-    if ( $scope.keywordsCanContinue() )
-    {
-      $state.go( 'collector-form.search-parameters' );
-    }
-  };
-  // End keywords
+        for (var i = 0; i < $scope.digest.keywords.length; i++) {
+            if (!$scope.digest.keywords[i].value) {
+                return false;
+            }
+        }
+        return true;
+    };
+    $scope.keywordsContinue    = function () {
+        if ($scope.keywordsCanContinue ()) {
+            $state.go ('collector-form.search-parameters');
+        }
+    };
+    // End keywords
 
 
-} ] );
+    // searchParameters
+    $scope.searchParametersCanContinue = function() {
+        // Out of range
+        if($scope.digest.searchParameters.resultsPerKeyword <= 0 || $scope.digest.searchParameters.resultsPerKeyword > 100) {
+            return false;
+        }
+
+        // Out of range
+        if($scope.digest.searchParameters.sortMethod < 0 || $scope.digest.searchParameters.sortMethod > 4) {
+            return false;
+        }
+
+        // Out of range
+        if($scope.digest.searchParameters.timePeriod < 0 || $scope.digest.searchParameters.timePeriod > 5) {
+            return false;
+        }
+
+        return true;
+    };
+
+    $scope.searchParametersContinue = function() {
+        if($scope.searchParametersCanContinue()) {
+            $state.go('collector-form.filterRules');
+        }
+    };
+    // End searchParameters
+
+
+}]);
--- assets/js/ng.js
+++ assets/js/ng.js
@@ -1,5 +1,13 @@
 var app = angular.module('app', ['ui.router', 'ngAnimate']);
 
+app.filter('capitalize', function() {
+    return function(input, scope) {
+        if (input!=null)
+            input = input.toLowerCase();
+        return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+});
+
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
     $locationProvider.html5Mode({enabled:true,requireBase:false});
@@ -20,12 +28,17 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
         .state('collector-form.keywords', {
             url: '/keywords',
-            templateUrl: '/pages/collector-form-keywords.html'
+            templateUrl: '/pages/collector-form/keywords.html'
         })
 
         .state('collector-form.search-parameters', {
             url: '/parameters',
-            templateUrl: '/pages/collector-form-search-parameters.html'
+            templateUrl: '/pages/collector-form/search-parameters.html'
+        })
+
+        .state('collector-form.filterRules', {
+            url: '/filters',
+            templateUrl: '/pages/collector-form/filters.html'
         })
 
     ;
